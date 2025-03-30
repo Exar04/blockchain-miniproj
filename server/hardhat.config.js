868aca80ac5aa4a144e0a4127090578e927e3441
Yash dhadve <yashdhadwe@gmail.com>
2025-03-30 15:37:55 +0530

    f

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -15,10 +15,8 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      // url: process.env.ALCHEMY_RINKEBY_URL,
-      // accounts: [process.env.ACCOUNT_PRIVATE_KEY],
-      url: "https://eth-sepolia.g.alchemy.com/v2/y47tYwP3MyVXk466csf0DcLpa03zlXT4",
-      accounts: ["30aa77404e396fa859289e54ad27819094fed908cf041bd147d32342b37b3d1d"],
+      url: process.env.ALCHEMY_RINKEBY_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     }
   }
 };
